15b38d7cdbfc92b3aa78cf1a864745a8cdd07693
RashingPro <rashingpro@yandex.ru>
2025-01-10 23:04:37 +0300

    Add core files

---
+++ src/type_writer.module.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import {useEffect, useState} from "react";
+import "./type_writer_module.css";
+
+interface TypeWriterProps {
+    strings: Array<string>,
+    isCursor?: boolean,
+    delayAfterDelete: number,
+    delayAfterPrint: number,
+    delayBetweenSymbol: number,
+    debug?: boolean,
+    wrapperClassName?: string,
+    cursorClassName?: string
+}
+export default function TypeWriter(
+    {
+        strings,
+        isCursor = true,
+        delayAfterDelete,
+        delayAfterPrint,
+        delayBetweenSymbol,
+        debug = false,
+        wrapperClassName = "type_writer_wrapper",
+        cursorClassName = "cursor"
+    }: TypeWriterProps
+) {
+    const [displayedText, setDisplayedText] = useState("")
+    const [currentTextIndex, setCurrentTextIndex] = useState(0);
+    const [isPaused, setIsPaused] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [delay, setDelay] = useState(0);
+    const update = () => {
+        if (isPaused) return;
+        if (delay > 0) {
+            return;
+        }
+        if (debug) console.debug(`calling update. isDeleting: ${isDeleting}; displayedText: ${displayedText}; currentText: ${strings[currentTextIndex]}`);
+        if (isDeleting) {
+            if (debug) console.debug(`deleting. slice: ${displayedText.slice(0, displayedText.length - 1)}`);
+            setDisplayedText((prevState: string) => {
+                return prevState.slice(0, prevState.length - 1)
+            })
+            if (displayedText === "") {
+                setIsDeleting(false);
+                setCurrentTextIndex((prevState: number) => {
+                    if (prevState + 1 < strings.length) {
+                        return prevState + 1;
+                    } else {
+                        return 0;
+                    }
+                })
+                setDelay(delayAfterDelete);
+                return;
+            }
+        } else {
+            if (debug) console.debug(`printing. currentTextIndex: ${strings[currentTextIndex][displayedText.length]}`)
+            setDisplayedText((prevState: string) => {
+                return prevState + strings[currentTextIndex][prevState.length]
+            })
+            if (displayedText === strings[currentTextIndex].slice(0, strings[currentTextIndex].length - 1)) {
+                setIsDeleting(true);
+                setDelay(delayAfterPrint);
+                return;
+            }
+        }
+        setDelay(delayBetweenSymbol);
+    }
+    useEffect(() => {
+        update();
+        const updateTimeout = setTimeout(() => {
+            setDelay((prevState) => {return prevState - 1})
+        }, 1)
+        return () => {
+            clearTimeout(updateTimeout);
+        }
+    }, [delay]);
+    return <span className={wrapperClassName}>
+        {displayedText}
+        {isCursor ? <span className={cursorClassName}>|</span> : <></>}
+    </span>
+}
